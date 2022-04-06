84206a8f5f30e868e3c8c62a596203b403bec811
Marcos Bort <marcosbort@gmail.com>
2022-04-06 16:40:01 -0300

    format

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import fetchWeather from "./api/fetchWeather";
-import './App.css';
-
+import "./App.css";
 
 function App() {
-
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
@@ -14,34 +12,34 @@ function App() {
       setWeather(data);
       setQuery("");
     }
-  }
+  };
 
   return (
-    <div className='main-container'>
-      <p className='autor'>By Marcos Bort</p>
+    <div className="main-container">
+      <p className="autor">By Marcos Bort</p>
 
       <input
         type="text"
-        className='search'
-        placeholder='Buscar... Ciudad, País'
+        className="search"
+        placeholder="Buscar... Ciudad, País"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
 
       {weather.main && (
-        <div className='city'>
-          <h2 className='city-name'>
+        <div className="city">
+          <h2 className="city-name">
             <span>{weather.name}</span>
             <sup>{weather.sys.country}</sup>
           </h2>
 
-          <div className='city-temp'>
+          <div className="city-temp">
             {Math.round(weather.main.temp)}
             <sup>&deg;C</sup>
           </div>
 
-          <div className='info'>
+          <div className="info">
             <img
               className="city-icon"
               src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -4,15 +4,15 @@ const URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_Key = "a9dcde741bb22d90f767d5de4853633d";
 
 const fetchWeather = async (query) => {
-    const {data} = await axios.get(URL, {
-        params: {
-            q: query,
-            units: "metric",
-            APPID: API_Key,
-        }
-    });
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_Key,
+    },
+  });
 
-    return data;
-}
+  return data;
+};
 
 export default fetchWeather;
