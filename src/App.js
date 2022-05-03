587224eef087aefeed5bc017913792a86b9e8174
Marcos Bort <marcosbort@gmail.com>
2022-05-03 01:10:15 -0300

    format and app update

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-import React, { useState } from "react";
-import fetchWeather from "./api/fetchWeather";
-import "./App.css";
+import React, { useState } from "react"
+import { fetchWeather } from "./services/fetchWeather"
+import "./App.css"
 
-function App() {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
+export const App = () => {
+  const [query, setQuery] = useState("")
+  const [weather, setWeather] = useState({})
 
   const search = async (e) => {
     if (e.key === "Enter") {
-      const data = await fetchWeather(query);
-      setWeather(data);
-      setQuery("");
+      const data = await fetchWeather(query)
+      setWeather(data)
+      setQuery("")
     }
-  };
+  }
 
   return (
     <div className="main-container">
       <p className="autor">By Marcos Bort</p>
-
       <input
         type="text"
         className="search"
-        placeholder="Buscar... Ciudad, País"
+        placeholder="Search... City, Country"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
-
       {weather.main && (
         <div className="city">
           <h2 className="city-name">
             <span>{weather.name}</span>
             <sup>{weather.sys.country}</sup>
           </h2>
-
           <div className="city-temp">
             {Math.round(weather.main.temp)}
             <sup>&deg;C</sup>
           </div>
-
           <div className="info">
             <img
               className="city-icon"
@@ -50,7 +46,7 @@ function App() {
         </div>
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/services/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const URL = "https://api.openweathermap.org/data/2.5/weather"
+const API_Key = "a9dcde741bb22d90f767d5de4853633d"
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_Key,
+    },
+  })
+
+  return data
+}
